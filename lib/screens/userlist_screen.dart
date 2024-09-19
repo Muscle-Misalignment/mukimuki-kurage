d82e8f003449b465bbaadd909166eead5f231728
umiyuri777 <yuseiai15@gmail.com>
2024-09-20 05:41:25 +0900

    feat: 目標を入れられるようにした

--- lib/screens/userlist_screen.dart
+++ lib/screens/userlist_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class UserlistScreen extends StatefulWidget {
   const UserlistScreen({Key? key}) : super(key: key);
@@ -8,14 +9,54 @@ class UserlistScreen extends StatefulWidget {
 }
 
 class _UserlistScreenState extends State<UserlistScreen> {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<List<Map<String, dynamic>>> fetchUsers() async {
+    try {
+      QuerySnapshot querySnapshot = await _firestore.collection('users').get();
+      final users = querySnapshot.docs.map((doc) => doc.data() as Map<String, dynamic>);
+      return users.toList();
+
+    } catch (e) {
+      print('Error fetching users: $e');
+    return [];
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('ユーザー一覧'),
-      ),
-      body: const Center(
-        child: Text('ユーザー一覧画面'),
+      appBar: AppBar(title: const Text('User List')),
+      body: FutureBuilder<List<Map<String, dynamic>>>(
+        future: fetchUsers(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text('No users found'));
+          } else {
+            final users = snapshot.data!;
+            return ListView.builder(
+              itemCount: users.length,
+              itemBuilder: (context, index) {
+                final user = users[index];
+                return ListTile(
+                  title: Text(user['username']),
+                  subtitle: Text(user['community']),
+                  leading:  CircleAvatar(
+                          backgroundImage: NetworkImage(user['photoURL']),
+                          onBackgroundImageError: (_, __) {
+                          // エラーが発生した場合の処理
+                          const Icon(Icons.account_circle);
+                        },
+                        ),
+                );
+              },
+            );
+          }
+        },
       ),
     );
   }
